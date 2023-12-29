018cac4394189a42eb03ab6bfa6ba997c1d28ab5
Rickyxrc <rickyxrc@outlook.com>
2023-12-29 20:15:29 +0800

    [Software Config]: kitty colorscheme everforest

--- home-manager/gui/kitty.nix
+++ home-manager/gui/kitty.nix
@@ -4,5 +4,7 @@
         enable = true;
         extraConfig = lib.fileContents ./kitty-config/kitty.conf;
     };
+
+    home.file.".config/kitty/everforest.conf".source = ./kitty-config/everforest.conf;
 }
 
