e7c3991569c5116fe2c3268a066dd32b25a07075
Rickyxrc <rickyxrc@outlook.com>
2023-12-31 15:21:25 +0800

    [Lint & Software config]: change dir format

---
+++ home-manager/cli/bash/default.nix
@@ -0,0 +1,6 @@
+{
+    # load starship config auto
+    home.file.".bashrc".source = ./bashrc;
+    # let login shell load this config
+    home.file.".bash_profile".source = ./bashrc;
+}
--- home-manager/cli/tmux.nix
+++
@@ -1,5 +0,0 @@
-{ pkgs, lib, ...} :
-{
-    programs.tmux.enable = true;
-    home.file.".tmux.conf".source = ./tmux-config/tmux.conf;
-}
