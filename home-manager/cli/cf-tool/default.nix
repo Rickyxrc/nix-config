72386bb49445c76cdb9cf333e0456bff6ee477b0
Rickyxrc <rickyxrc@outlook.com>
2024-01-06 18:22:27 +0800

    [Software ]

--- home-manager/cli/cf-tool/default.nix
+++ home-manager/cli/cf-tool/default.nix
@@ -1,4 +1,4 @@
-{inputs, config, options, modulesPath, outputs, lib, specialArgs, pkgs} : {
+{ inputs, config, options, modulesPath, outputs, lib, specialArgs, pkgs } : {
     home.packages = [ inputs.cf-tool.packages.${pkgs.system}.default ]; # flake cf-tool packaged by rickyxrc
     home.file.".cf/config".source = ./config;
     home.file."oi/template.cpp".source = ./template.cpp;
