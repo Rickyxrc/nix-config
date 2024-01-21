fa3a70266cacc745e90112f89792697204385703
Rickyxrc <rickyxrc@outlook.com>
2024-01-22 01:25:26 +0800

    feat(neovide): change theme

--- home-manager/cli/lazyvim/conf/lua/config/options.lua
+++ home-manager/cli/lazyvim/conf/lua/config/options.lua
@@ -12,9 +12,13 @@ local alpha = function()
 end
 if vim.g.neovide then
     vim.g.neovide_scale_factor = 0.75
-    vim.g.neovide_transparency = 0.9
-    vim.g.transparency = 0.8
+    vim.g.neovide_transparency = 0.8
+    vim.g.transparency = 0.6
     vim.g.neovide_background_color = "#0f1117" .. alpha()
+    vim.g.neovide_window_blurred = true
+    vim.g.neovide_floating_blur_amount_x = 8.0
+    vim.g.neovide_floating_blur_amount_y = 8.0
+    vim.g.neovide_hide_mouse_when_typing = true
 end
 
 vim.g.mapleader = " "
