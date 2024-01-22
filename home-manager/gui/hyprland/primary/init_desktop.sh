abda52f3274cc5ef7b3a049d6014f98778791e3c
Rickyxrc <rickyxrc@outlook.com>
2024-01-22 20:15:51 +0800

    feat(Hyprland): change init_desktop.sh

--- home-manager/gui/hyprland/primary/init_desktop.sh
+++ home-manager/gui/hyprland/primary/init_desktop.sh
@@ -3,7 +3,7 @@ tmux kill-session -t daemon
 tmux kill-session -t proxy
 tmux kill-session -t musicfox
 
-export INTERVAL=0.3
+export INTERVAL=0.1
 
 # Music
 tmux new-session -d -s musicfox -n musicfox -d "musicfox"
@@ -21,28 +21,45 @@ tmux split-window -t daemon -d "start-pulseaudio-x11"
 # proxy
 tmux new-session -d -s proxy -d "sudo v2rayA"
 
-# Launch Applications
-kitty &
+hyprctl dispatch workspace 1
 sleep $INTERVAL
-hyprctl dispatch movetoworkspace 2
-sleep $INTERVAL
-
-firefox &
-sleep $INTERVAL
-hyprctl dispatch movetoworkspace 6
-sleep $INTERVAL
-
-discord &
-sleep $INTERVAL
-hyprctl dispatch movetoworkspace 2
-sleep $INTERVAL
-
 neovide &
 sleep $INTERVAL
-hyprctl dispatch movetoworkspace 1
+kitty tmux attach-session -t proxy & # prompt for password, so focus on this
 sleep $INTERVAL
 
-kitty tmux attach-session -t proxy & # prompt for password, so focus on this
+hyprctl dispatch workspace 2
+sleep $INTERVAL
+discord &
+sleep $INTERVAL
+
+hyprctl dispatch workspace 3
+sleep $INTERVAL
+
+hyprctl dispatch workspace 4
+sleep $INTERVAL
+
+hyprctl dispatch workspace 5
+sleep $INTERVAL
+
+hyprctl dispatch workspace 6
+sleep $INTERVAL
+firefox &
+sleep $INTERVAL
+
+hyprctl dispatch workspace 7
+sleep $INTERVAL
+
+hyprctl dispatch workspace 8
+sleep $INTERVAL
+
+hyprctl dispatch workspace 9
+sleep $INTERVAL
+
+hyprctl dispatch workspace 6
+sleep $INTERVAL
+hyprctl dispatch workspace 1
+sleep $INTERVAL
 
 sleep 15                         # wait musicfox loading + proxy booting
 tmux send-keys -t musicfox space # start playing
