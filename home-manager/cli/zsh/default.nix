e8ea9f39af117e18285acccd29255c5f6f779e1c
Rickyxrc <rickyxrc@outlook.com>
2024-01-23 19:29:37 +0800

    feat(zsh): auto launch Hyprland at tty1

--- home-manager/cli/zsh/default.nix
+++ home-manager/cli/zsh/default.nix
@@ -7,5 +7,13 @@
                 { name = "zsh-users/zsh-autosuggestions"; }
             ];
         };
+        initExtra = ''
+            # Launch Hyprland at tty1
+            if [[ $TTY == /dev/tty1 ]]; then
+                Hyprland;
+            fi
+
+            # Other package generated hook
+        '';
     };
 }
