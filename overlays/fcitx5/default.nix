284aa010c0964809f26d29985637b486353b3cc6
Rickyxrc <rickyxrc@outlook.com>
2024-04-26 19:44:02 +0800

    waiting build...

--- overlays/fcitx5/default.nix
+++ overlays/fcitx5/default.nix
@@ -4,9 +4,11 @@
 (_: super: {
   # 小鹤音形配置，配置来自 flypy.com 官方网盘的鼠须管配置压缩包「小鹤音形“鼠须管”for macOS.zip」
   # 我仅修改了 default.yaml 文件，将其中的半角括号改为了直角括号「 与 」。
-  rime-data = ./rime-data-flypy;
-  fcitx5-rime = super.fcitx5-rime.override { rimeDataPkgs = [ ./rime-data-flypy ]; };
+  # rime-data = ./rime-data-flypy;
+  rime-data = ./rime-data-huma;
+  # fcitx5-rime = super.fcitx5-rime.override { rimeDataPkgs = [ ./rime-data-flypy ]; };
+  fcitx5-rime = super.fcitx5-rime.override { rimeDataPkgs = [ ./rime-data-huma ]; };
 
   # used by macOS Squirrel
-  flypy-squirrel = ./rime-data-flypy;
+  # flypy-squirrel = ./rime-data-huma;
 })
